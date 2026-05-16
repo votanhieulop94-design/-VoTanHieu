01e567e7b8c9d4c98ae51d978b87d63928fe49a4
Híu Bún Bòa <votanhieulop94@gmail.com>
2026-05-16 16:32:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,40 @@ window.addEventListener("scroll", () => {
 
   if(window.scrollY > 50){
 
-    navbar.style.background = "rgba(2,6,23,0.85)";
+    navbar.style.background = "rgba(2,6,23,0.9)";
 
   }else{
 
     navbar.style.background = "transparent";
   }
+});
+
+/* SMOOTH SCROLL */
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+
+  link.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+
+    const targetSection = document.querySelector(targetId);
+
+    const navbar = document.querySelector('.navbar');
+
+    const navbarHeight = navbar.offsetHeight;
+
+    const sectionTop =
+      targetSection.offsetTop - navbarHeight - 30;
+
+    window.scrollTo({
+
+      top: sectionTop,
+
+      behavior: 'smooth'
+    });
+  });
 });
